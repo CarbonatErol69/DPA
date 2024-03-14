89c383bb7cae8eae04e82b26b0b684514505b0ba
Schoeberichts <Tim.Schoeberichts@saint-gobain.com>
2024-03-14 13:25:48 +0100

    reverted to old erts logic

--- test/AlexGPT.py
+++ test/AlexGPT.py
@@ -2,39 +2,146 @@ from collections import defaultdict
 import numpy as np
 import pandas as pd
 import random
+import math
 
 class AlexGPT():
     def __init__(self, schueler_wahlen_path, raumliste_path, veranstaltungsliste_path):
         #self.main()
         self.schueler_wahlen_path = schueler_wahlen_path
         self.raumliste_path = raumliste_path
-        self.veranstaltungsliste_path = veranstaltungsliste_path    
+        self.veranstaltungsliste_path = veranstaltungsliste_path    # Hola
     
 
     def main(self):
+        self.create_dataframes()
+        self.count_votes()
+        self.create_event_df()
+        self.create_timetable()
+        '''
         print(self.schueler_wahlen_path)
         self.create_dataframes()
         self.prepare_veranstaltungsliste()
         self.calculate_event_demand()
-        self.assign_rooms_to_companies()
-        self.assign_rooms_to_events_based_on_capacity()
-        self.estimate_event_count()
+        #self.assign_rooms_to_companies()
+        #self.assign_rooms_to_events_based_on_capacity()
+        #self.estimate_event_count()
         self.assign_events_to_rooms()
-        self.assign_students_to_events()
-        self.complete_student_assignments()
-        self.assign_remaining_students()
-        self.export_results()
+        #self.assign_students_to_events()
+        #self.complete_student_assignments()
+        #self.assign_remaining_students()
+        #self.export_results()
+        '''
 
     
     def create_dataframes(self):
         """Lädt die Daten aus den Excel-Dateien."""
-        self.schuelerwahlen_df = pd.read_excel(self.schueler_wahlen_path).reset_index().rename(columns={'index': 'SchuelerID'})
-        self.raumliste_df = pd.read_excel(self.raumliste_path)
-        self.veranstaltungsliste_df =  pd.read_excel(self.veranstaltungsliste_path)
+        self.df_wahl = pd.read_excel(self.schueler_wahlen_path).reset_index().rename(columns={'index': 'SchuelerID'})
+        self.df_raum = pd.read_excel(self.raumliste_path)
+        self.df_event =  pd.read_excel(self.veranstaltungsliste_path)
     
-        print('Raumliste:', self.raumliste_df.columns)
-        print('Veranstaltungsliste:', self.veranstaltungsliste_df.columns)
-        print('Schülerwahlen:', self.schuelerwahlen_df.columns)
+        print('Raumliste:', self.df_raum.columns)
+        print('Veranstaltungsliste:', self.df_event.columns)
+        print('Schülerwahlen:', self.df_wahl.columns)
+
+    def count_votes(self):
+        self.value_counts = pd.concat([self.df_wahl['Wahl 1'].value_counts(),
+                         self.df_wahl['Wahl 2'].value_counts(),
+                         self.df_wahl['Wahl 3'].value_counts(),
+                         self.df_wahl['Wahl 4'].value_counts(),
+                         self.df_wahl['Wahl 5'].value_counts(),
+                         self.df_wahl['Wahl 6'].value_counts()], axis=1)
+
+        column_names = ['Wahl1', 'Wahl2', 'Wahl3', 'Wahl4', 'Wahl5', 'Wahl6']
+        self.value_counts.columns = column_names
+
+        self.value_counts.insert(0, 'event_number', self.value_counts.index)
+
+    def create_event_df(self):
+        self.df_event = self.df_event.rename(columns={'Nr. ': 'event_number'})
+
+        self.merged_df = pd.merge(self.df_event, self.value_counts, on='event_number')
+
+        self.merged_df['Total 1-3'] = self.merged_df[['Wahl1', 'Wahl2', 'Wahl3']].sum(axis=1).astype(int)
+
+        self.merged_df['Total 4-6'] = self.merged_df[['Wahl4', 'Wahl5', 'Wahl6']].sum(axis=1).astype(int)
+
+        self.merged_df['Total'] = self.merged_df['Total 1-3'] + self.merged_df['Total 4-6']
+
+        self.merged_df['Nötige Slots'] = self.merged_df['Total 1-3'] / self.merged_df['Max. Teilnehmer']
+
+        self.merged_df['Optionale Slots'] = self.merged_df['Total 4-6'] / self.merged_df['Max. Teilnehmer']
+
+        threshhold = 5 / self.merged_df['Max. Teilnehmer']
+
+        # Round the 'Nötige Slots' column up or down
+        self.merged_df['Rundet Nötige Slots'] = self.merged_df['Nötige Slots'].apply(lambda x: math.ceil(x) if x - math.floor(x) >= 0.25 else math.floor(x))
+
+        # Round the 'Optionale Slots' column up or down
+        self.merged_df['Rundet Optionale Slots'] = self.merged_df['Optionale Slots'].apply(lambda x: math.ceil(x) if x - math.floor(x) >= 0.25 else math.floor(x))
+
+    def create_timetable(self):
+        self.df_slots = pd.DataFrame(columns=['A', 'B', 'C', 'D', 'E'])
+        self.df_timetable = pd.concat([self.df_raum, self.df_slots[['A', 'B', 'C', 'D', 'E']]], axis=1)
+
+        # Sort df_merged by 'Total 1-3' in descending order
+        self.df_merged_sorted = self.merged_df.sort_values('Total 1-3', ascending=False)
+
+        # Iterate over the sorted events
+        for _, event in self.df_merged_sorted.iterrows():
+            event_number = event['event_number']
+            rundet_slots = event['Rundet Nötige Slots']
+            
+            # Find the room with the least number of events already assigned
+            min_events_room = self.df_timetable.iloc[:, 1:].apply(lambda row: row.notnull().sum(), axis=1).idxmin()
+            
+            # Assign the event to the room with the least number of events
+            room_row = self.df_timetable.loc[min_events_room]
+            available_slots = rundet_slots - room_row[1:].eq(event_number).sum()
+            available_columns = room_row[1:][room_row[1:].isnull()].index.tolist()
+            for i in range(min(available_slots, len(available_columns))):
+                self.df_timetable.loc[min_events_room, available_columns[i]] = event_number
+
+        #TODO: check for frühester startzeitpunkt
+                
+        #TODO: check for same company in ONE room 
+                
+        #TODO: 
+
+        print(self.df_timetable)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        '''
         
 
     def prepare_veranstaltungsliste(self):
@@ -52,16 +159,23 @@ class AlexGPT():
                 if pd.notna(event_id):
                     event_demand[event_id] += 1
                 
+    def assign_rooms_to_events(self):
+        pass
+    
     def assign_rooms_to_companies(self):
         """Weist Räume Unternehmen und Fachrichtungen zu."""
-        raum_zuweisung = {}
+        raum_zuweisung = {} #was dict before
         unternehmen_fachrichtungen = defaultdict(set)
         for _, veranstaltung in self.veranstaltungsliste_df.iterrows():
+            print("verstanstaltung unternehmen: ", veranstaltung['Unternehmen'])
             unternehmen = veranstaltung['Unternehmen'].strip()
             fachrichtung = veranstaltung['Fachrichtung']
-            raum = self.raumliste_df.loc[self.raumliste_df['Unternehmen'] == unternehmen, 'Raum'].iloc[0]
-            raum_zuweisung[(unternehmen, fachrichtung)] = raum
-            unternehmen_fachrichtungen[unternehmen].add(fachrichtung)
+            print("raumliste unternehmen:", self.raumliste_df.columns)
+            raum = self.raumliste_df.loc[self.veranstaltungsliste_df['Unternehmen'] == unternehmen, 'Raum'].iloc[0]
+            print("raum:", raum)
+            raum_zuweisung[(unternehmen, fachrichtung)].append(raum)
+            print("raum_zuweisung", raum_zuweisung)
+            unternehmen_fachrichtungen[unternehmen].add(fachrichtung) 
 
     def assign_rooms_to_events_based_on_capacity(self):
         """Weist Räume zu Veranstaltungen basierend auf der Kapazität zu."""
@@ -92,6 +206,7 @@ class AlexGPT():
             estimated_count = self.estimated_event_count[unternehmen]
             for _ in range(int(estimated_count)):
                 raum_event_zuweisung[(unternehmen, raum)].append(event_id)
+        print("raum-event zuweisung", raum_event_zuweisung)
 
     def assign_students_to_events(self):
         """Weist Schüler zu Veranstaltungen zu."""
@@ -158,3 +273,4 @@ class AlexGPT():
         
         print(export_df)
 
+'''
